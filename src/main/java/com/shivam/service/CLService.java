e08549740e71c1b7b699b12235304becbef78e14
shivamm <thakreshivam48@gmail.com>
2025-01-07 13:33:57 +0530

    Your commit message

--- src/main/java/com/shivam/service/CLService.java
+++ src/main/java/com/shivam/service/CLService.java
@@ -6,6 +6,33 @@ import com.shivam.model.divideByZeroException;
 
 @Service
 public  class CLService implements calcuservice {
+  
+	public double calculate(double a, double b, String operation) {
+		switch (operation.toLowerCase()) { 
+			case "multiply":
+			return a * b; 
+
+			case "divide":
+			return a / b; 
+	   
+			case "add":
+			return a+b;
+		
+			case  "sub":
+			return a-b;
+			
+			case "madulo":
+			return a%b;
+			
+			case "sin":
+			return Math.sin(Math.toRadians(a));  
+			
+			case "cos":
+			return Math.cos(Math.toRadians(a));
+
+	}
+		return b;
+}
 
 	public double add(double a, double b) {
 		 return a+b;
